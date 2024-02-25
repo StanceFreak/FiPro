76aa81ce8f152d8a1614bf47025e141c94c7f37c
StanceFreak <jombor.army67@gmail.com>
2024-02-25 17:57:53 +0700

    feat[tablayout content]: added both monitoring performance and physical fragment inside the tablayout

---
+++ app/src/main/java/com/stancefreak/monkob/views/monitoring/MonitoringPhysicalFragment.kt
@@ -0,0 +1,31 @@
+package com.stancefreak.monkob.views.monitoring
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.stancefreak.monkob.databinding.FragmentMonitoringPhysicalBinding
+
+class MonitoringPhysicalFragment : Fragment() {
+
+    private lateinit var binding: FragmentMonitoringPhysicalBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentMonitoringPhysicalBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+//        TODO("Not yet implemented")
+    }
+
+}
