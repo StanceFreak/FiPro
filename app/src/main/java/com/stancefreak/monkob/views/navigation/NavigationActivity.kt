378fdb018d2d9e1f1c2f9b9cd5c2178b04dc1b50
StanceFreak <jombor.army67@gmail.com>
2024-05-05 23:36:01 +0700

    bug fix[fcm & mem leak]: fixed the bug that causing the notification didnt show up and remove the unused navcontroller lines

--- app/src/main/java/com/stancefreak/monkob/views/navigation/NavigationActivity.kt
+++ app/src/main/java/com/stancefreak/monkob/views/navigation/NavigationActivity.kt
@@ -2,8 +2,11 @@ package com.stancefreak.monkob.views.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.fragment.NavHostFragment
-import com.stancefreak.monkob.R
+import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.ktx.messaging
 import com.stancefreak.monkob.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,12 +18,26 @@ class NavigationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setupNav()
-    }
 
-    private fun setupNav() {
-        val navHost = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
-        navHost.navController
+        Firebase.messaging.subscribeToTopic("all").addOnCompleteListener { task ->
+            var msg = "Subscribed"
+            if (!task.isSuccessful) {
+                msg = "Subscribe failed"
+            }
+            Log.d("test topic", msg)
+        }
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            Log.d("token fcm", token)
+        })
     }
 
 
